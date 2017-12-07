a7be13a07d708a86cdfe2ea19228dbb774f271d4
Denis Bystruev <dbystruev@me.com>
2017-12-07 13:42:42 +0300

    Initial Commit

--- Guided Project — Light/ViewController.swift
+++ Guided Project — Light/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        view.backgroundColor = .black
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
